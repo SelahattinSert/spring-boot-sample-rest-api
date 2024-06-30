5365a9cb54148121a2a30b21b0c1f9c2ac27e7f5
Selahattin Sert <selahattinsert25@gmail.com>
2024-06-30 18:46:29 +0300

    Sprin Boot project for camera onboarding REST API

--- camera-onboarding/src/main/java/com/example/camera_onboarding/dao/CameraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.camera_onboarding.dao;
-
-import com.example.camera_onboarding.entity.Camera;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CameraRepository extends JpaRepository<Camera, Integer> {
-}
