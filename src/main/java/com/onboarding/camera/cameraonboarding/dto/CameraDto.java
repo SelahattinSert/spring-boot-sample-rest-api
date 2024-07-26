9d9f76613097e5751a8a44804ca1f302994191e3
Selahattin Sert <selahattinsert25@gmail.com>
2024-07-26 03:12:08 +0300

    Add spring validation

--- src/main/java/com/onboarding/camera/cameraonboarding/dto/CameraDto.java
+++ src/main/java/com/onboarding/camera/cameraonboarding/dto/CameraDto.java
@@ -1,5 +1,7 @@
 package com.onboarding.camera.cameraonboarding.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CameraDto {
+    @NotBlank(message = "Camera name cannot be blank")
+    @Size(min = 2, max = 100, message = "Camera name should be up to 100 characters")
     private String cameraName;
+
+    @NotBlank(message = "Firmware version cannot be blank")
+    @Size(min = 2, max = 100, message = "Firmware version should be up to 100 characters")
     private String firmwareVersion;
 }
