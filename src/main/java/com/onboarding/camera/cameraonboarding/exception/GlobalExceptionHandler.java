0eac99047c8bc1808c373740ce904da46cc679d3
Selahattin Sert <selahattinsert25@gmail.com>
2024-07-26 03:09:17 +0300

    Add new exception handling

--- src/main/java/com/onboarding/camera/cameraonboarding/exception/GlobalExceptionHandler.java
+++ src/main/java/com/onboarding/camera/cameraonboarding/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.onboarding.camera.cameraonboarding.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +23,19 @@ public class GlobalExceptionHandler {
         errorResponse.setTimestamp(new Date());
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+
+        errorResponse.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setTimestamp(new Date());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
 
