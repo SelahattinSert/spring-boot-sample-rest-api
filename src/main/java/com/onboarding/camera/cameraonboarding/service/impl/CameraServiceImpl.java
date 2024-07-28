67a89cebae241d675ef308e4e8d40858e74cba1d
Selahattin Sert <selahattinsert25@gmail.com>
2024-07-28 14:39:49 +0300

    Add created_at date

--- src/main/java/com/onboarding/camera/cameraonboarding/service/impl/CameraServiceImpl.java
+++ src/main/java/com/onboarding/camera/cameraonboarding/service/impl/CameraServiceImpl.java
@@ -1,12 +1,14 @@
-package com.onboarding.camera.cameraonboarding.service;
+package com.onboarding.camera.cameraonboarding.service.impl;
 
 import com.onboarding.camera.cameraonboarding.dao.CameraRepository;
 import com.onboarding.camera.cameraonboarding.entity.Camera;
 import com.onboarding.camera.cameraonboarding.exception.CameraNotCreatedException;
 import com.onboarding.camera.cameraonboarding.rest.CameraRestController;
+import com.onboarding.camera.cameraonboarding.service.CameraService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
 @Service
 public class CameraServiceImpl implements CameraService {
@@ -23,6 +25,7 @@ public class CameraServiceImpl implements CameraService {
     public Camera handleSaveCamera(Camera camera) {
 
         try {
+            camera.setCreatedAt(LocalDateTime.now());
             Camera savedCamera = cameraRepository.save(camera);
             logger.info("Saved camera with ID: {}", savedCamera.getCamId());
             return savedCamera;
