6a5fe5ff9dd68344a59d958f74868132e171a81e
Selahattin Sert <selahattinsert25@gmail.com>
2024-07-29 19:17:41 +0300

    Add DateTimeFactory service

---
+++ src/main/java/com/onboarding/camera/cameraonboarding/service/DateTimeFactory.java
@@ -0,0 +1,14 @@
+package com.onboarding.camera.cameraonboarding.service;
+
+import java.time.LocalDateTime;
+
+public interface DateTimeFactory {
+
+    /**
+     * this method is used for get current time
+     *
+     * @return localDateTime
+     */
+
+    LocalDateTime now();
+}
---
+++ src/main/java/com/onboarding/camera/cameraonboarding/service/impl/DateTimeFactoryImpl.java
@@ -0,0 +1,23 @@
+package com.onboarding.camera.cameraonboarding.service.impl;
+
+import com.onboarding.camera.cameraonboarding.service.DateTimeFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+@Service
+public class DateTimeFactoryImpl implements DateTimeFactory {
+
+    private final Clock clock;
+
+    @Autowired
+    public DateTimeFactoryImpl(Clock clock) {
+        this.clock = clock;
+    }
+
+    @Override
+    public LocalDateTime now() {
+        return LocalDateTime.now(clock);
+    }
+}
