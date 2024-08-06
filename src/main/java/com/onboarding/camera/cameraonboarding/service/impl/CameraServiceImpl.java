c2e0e757caaa1be4a8a450cb4e6bdc57ae27b431
Selahattin Sert <selahattinsert25@gmail.com>
2024-08-07 01:47:59 +0300

    Rename exception names

--- src/main/java/com/onboarding/camera/cameraonboarding/service/impl/CameraServiceImpl.java
+++ src/main/java/com/onboarding/camera/cameraonboarding/service/impl/CameraServiceImpl.java
@@ -1,24 +1,22 @@
 package com.onboarding.camera.cameraonboarding.service.impl;
 
+import com.onboarding.camera.cameraonboarding.exception.CameraAlreadyInitializedException;
 import com.onboarding.camera.cameraonboarding.exception.CameraNotFoundException;
+import com.onboarding.camera.cameraonboarding.exception.CameraNotInitializedException;
 import com.onboarding.camera.cameraonboarding.repository.CameraRepository;
 import com.onboarding.camera.cameraonboarding.entity.Camera;
 import com.onboarding.camera.cameraonboarding.exception.CameraNotCreatedException;
-import com.onboarding.camera.cameraonboarding.rest.CameraRestController;
 import com.onboarding.camera.cameraonboarding.service.CameraService;
 import com.onboarding.camera.cameraonboarding.service.DateTimeFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CameraServiceImpl implements CameraService {
 
     private final CameraRepository cameraRepository;
-
-    private static final Logger logger = LoggerFactory.getLogger(CameraRestController.class);
 
     private final DateTimeFactory dateTimeFactory;
 
@@ -33,10 +31,10 @@ public class CameraServiceImpl implements CameraService {
         try {
             camera.setCreatedAt(dateTimeFactory.now());
             Camera savedCamera = cameraRepository.save(camera);
-            logger.info("Saved camera with ID: {}", savedCamera.getCamId());
+            log.info("Saved camera with ID: {}", savedCamera.getCamId());
             return savedCamera;
         } catch (Exception ex) {
-            logger.error("Exception occurred while saving camera: {}", ex.getMessage());
+            log.error("Exception occurred while saving camera: {}", ex.getMessage());
             throw new CameraNotCreatedException("Error occurred while saving camera: " + ex.getMessage());
         }
     }
@@ -46,17 +44,17 @@ public class CameraServiceImpl implements CameraService {
         try {
             Camera camera = getCameraById(cameraId);
             if (camera.getInitializedAt() != null  && !camera.getInitializedAt().toString().isBlank()) {
-                throw new RuntimeException("Camera already initialized");
+                throw new CameraAlreadyInitializedException("Camera already initialized");
             }
             camera.setInitializedAt(dateTimeFactory.now());
             cameraRepository.save(camera);
-            logger.info("Initialized camera with ID: {}", cameraId);
+            log.info("Initialized camera with ID: {}", cameraId);
         } catch (CameraNotFoundException ex) {
-            logger.error("Camera not found with ID: {}", cameraId, ex);
+            log.error("Camera not found with ID: {}", cameraId, ex);
             throw ex;
-        } catch (Exception ex) {
-            logger.error("Exception occurred while initializing camera with ID: {}", cameraId, ex);
-            throw new RuntimeException("Error occurred while initializing camera: " + ex.getMessage(), ex);
+        } catch (CameraNotInitializedException ex) {
+            log.error("Exception occurred while initializing camera with ID: {}", cameraId, ex);
+            throw new CameraNotInitializedException("Error occurred while initializing camera: " + ex.getMessage());
         }
     }
 
@@ -66,17 +64,8 @@ public class CameraServiceImpl implements CameraService {
             throw new IllegalArgumentException("Camera ID cannot be null");
         }
 
-        Optional<Camera> result = cameraRepository.findById(cameraId);
-
-        Camera camera;
-
-        if (result.isPresent()) {
-            camera = result.get();
-        } else {
-            throw new CameraNotFoundException("Camera not found with id: " + cameraId);
-        }
-
-        return camera;
+        return cameraRepository.findById(cameraId)
+                .orElseThrow(() -> new CameraNotFoundException("Camera not found with id: " + cameraId));
     }
 
 }
