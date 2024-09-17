d201ec70abc20cf29112ee8c4be698459fb0fde0
Selahattin Sert <selahattinsert25@gmail.com>
2024-09-17 21:22:29 +0300

    Remove final annotations, fix mediatype for handleUploadImage and Add validation method

--- src/main/java/com/onboarding/camera/cameraonboarding/service/impl/CameraServiceImpl.java
+++ src/main/java/com/onboarding/camera/cameraonboarding/service/impl/CameraServiceImpl.java
@@ -1,6 +1,5 @@
 package com.onboarding.camera.cameraonboarding.service.impl;
 
-import com.onboarding.camera.cameraonboarding.dto.UploadImageDto;
 import com.onboarding.camera.cameraonboarding.entity.Camera;
 import com.onboarding.camera.cameraonboarding.exception.CameraAlreadyInitializedException;
 import com.onboarding.camera.cameraonboarding.exception.CameraNotCreatedException;
@@ -12,46 +11,41 @@ import com.onboarding.camera.cameraonboarding.repository.CameraRepository;
 import com.onboarding.camera.cameraonboarding.service.BlobStorageService;
 import com.onboarding.camera.cameraonboarding.service.CameraService;
 import com.onboarding.camera.cameraonboarding.util.DateTimeFactory;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class CameraServiceImpl implements CameraService {
 
     private final CameraRepository cameraRepository;
 
-    private final BlobStorageService blobStorageService;
-
     private final DateTimeFactory dateTimeFactory;
 
-    public CameraServiceImpl(final CameraRepository cameraRepository, final DateTimeFactory dateTimeFactory, final BlobStorageService blobStorageService) {
-        this.cameraRepository = cameraRepository;
-        this.dateTimeFactory = dateTimeFactory;
-        this.blobStorageService = blobStorageService;
-    }
+    private final BlobStorageService blobStorageService;
 
     @Override
-    public Camera handleSaveCamera(final Camera camera) {
+    public Camera handleSaveCamera(Camera camera) {
 
         try {
             camera.setCreatedAt(dateTimeFactory.now());
             camera.setOnboardedAt(dateTimeFactory.now());
-            final Camera savedCamera = cameraRepository.save(camera);
+            Camera savedCamera = cameraRepository.save(camera);
             log.info("Camera saved with ID: {}", savedCamera.getCamId());
             return savedCamera;
-        } catch (final Exception ex) {
+        } catch (Exception ex) {
             log.error("Exception occurred while saving camera: {}", ex.getMessage());
             throw new CameraNotCreatedException("Error occurred while saving camera: " + ex.getMessage());
         }
     }
 
     @Override
-    public void handleInitializeCamera(final UUID cameraId) {
-        final Camera camera = getCameraById(cameraId);
+    public void handleInitializeCamera(UUID cameraId) {
+        Camera camera = getCameraById(cameraId);
         if (camera.getInitializedAt() != null && !camera.getInitializedAt().toString().isBlank()) {
             throw new CameraAlreadyInitializedException("Camera already initialized");
         }
@@ -60,13 +54,13 @@ public class CameraServiceImpl implements CameraService {
             camera.setInitializedAt(dateTimeFactory.now());
             cameraRepository.save(camera);
             log.info("Camera initialized with ID: {}", cameraId);
-        } catch (final Exception ex) {
+        } catch (Exception ex) {
             log.error("Exception occurred while initializing camera with ID: {}", cameraId, ex);
             throw new CameraNotInitializedException("Error occurred while initializing camera: " + ex.getMessage());
         }
     }
 
-    public Camera getCameraById(final UUID cameraId) {
+    public Camera getCameraById(UUID cameraId) {
 
         if (cameraId == null) {
             throw new IllegalArgumentException("Camera ID cannot be null");
@@ -77,16 +71,11 @@ public class CameraServiceImpl implements CameraService {
     }
 
     @Override
-    public void handleUploadImage(final UUID cameraId, final UploadImageDto uploadImageDto) {
-        final Camera camera = getCameraById(cameraId);
-        if (camera.getOnboardedAt() == null || camera.getOnboardedAt().toString().isBlank()) {
-            throw new CameraNotFoundException("Camera is not onboarded with id: " + cameraId);
-        } else if (camera.getInitializedAt() == null || camera.getInitializedAt().toString().isBlank()) {
-            throw new CameraNotInitializedException("Camera is not initialized with id: " + cameraId);
-        }
-        final MultipartFile file = uploadImageDto.getFile();
-        final UUID imageId = uploadImageDto.getImageId();
-        log.info("Received file:{}", file.getOriginalFilename());
+    public void handleUploadImage(UUID cameraId, UUID imageId, byte[] imageData) {
+        Camera camera = getCameraById(cameraId);
+        validateCameraImageUpload(camera);
+
+        log.info("Received file:{}", imageId);
         try {
             if (camera.getImageId() != null) {
                 throw new ImageAlreadyUploadedException("Camera already have image with id: " + camera.getImageId());
@@ -94,13 +83,28 @@ public class CameraServiceImpl implements CameraService {
             camera.setImageId(imageId);
             cameraRepository.save(camera);
             log.info("Uploading image with ID: {}", imageId);
-            blobStorageService.uploadFile(blobStorageService.getContainerName(), imageId.toString(), file.getBytes());
-        } catch (final ImageAlreadyUploadedException ex) {
+            blobStorageService.uploadFile(blobStorageService.getContainerName(), imageId.toString(), imageData);
+        } catch (ImageAlreadyUploadedException ex) {
             log.error("Exception occurred while uploading image");
             throw new ImageAlreadyUploadedException("Camera already have image with id: " + camera.getImageId());
-        } catch (final Exception ex) {
-            log.error("Exception occurred while uploading image:{}:ex:{}", uploadImageDto.getFile().getOriginalFilename(), ex.getMessage());
+        } catch (Exception ex) {
+            log.error("Exception occurred while uploading image:{}:ex:{}", imageId, ex.getMessage());
             throw new ImageNotUploadedException("Error occurred while uploading image: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * Validates if the camera has been onboarded and initialized
+     *
+     * @param camera the camera to check
+     * @throws CameraNotFoundException       if the camera is not onboarded
+     * @throws CameraNotInitializedException if the camera is not initialized
+     */
+    private void validateCameraImageUpload(Camera camera) {
+        if (camera.getOnboardedAt() == null || camera.getOnboardedAt().toString().isBlank()) {
+            throw new CameraNotFoundException("Camera is not onboarded with id: " + camera.getCamId());
+        } else if (camera.getInitializedAt() == null || camera.getInitializedAt().toString().isBlank()) {
+            throw new CameraNotInitializedException("Camera is not initialized with id: " + camera.getCamId());
         }
     }
 }
