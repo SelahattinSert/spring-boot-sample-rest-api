b1ba4d0d53f3886da43ee733f75db779da45d4d7
Selahattin Sert <selahattinsert25@gmail.com>
2024-10-30 15:52:29 +0300

    Add dto for location object

---
+++ src/main/java/com/onboarding/camera/cameraonboarding/dto/LocationDto.java
@@ -0,0 +1,11 @@
+package com.onboarding.camera.cameraonboarding.dto;
+
+import lombok.Data;
+
+@Data
+public class LocationDto {
+
+    private double latitude;
+    private double longitude;
+    private String address;
+}
