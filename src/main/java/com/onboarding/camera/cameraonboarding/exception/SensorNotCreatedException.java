a4f0f674f49e1988b3b31487eafe636dfb134740
Selahattin Sert <selahattinsert25@gmail.com>
2024-12-09 02:34:23 +0300

    Add sensor exceptions

---
+++ src/main/java/com/onboarding/camera/cameraonboarding/exception/SensorNotCreatedException.java
@@ -0,0 +1,7 @@
+package com.onboarding.camera.cameraonboarding.exception;
+
+public class SensorNotCreatedException extends RuntimeException {
+    public SensorNotCreatedException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/onboarding/camera/cameraonboarding/exception/SensorNotUpdatedException.java
@@ -0,0 +1,7 @@
+package com.onboarding.camera.cameraonboarding.exception;
+
+public class SensorNotUpdatedException extends RuntimeException {
+    public SensorNotUpdatedException(String message) {
+        super(message);
+    }
+}
