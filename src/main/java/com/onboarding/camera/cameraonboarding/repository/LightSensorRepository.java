37e3f5ad6da0cdea5f652a377f129aff3018089d
Selahattin Sert <selahattinsert25@gmail.com>
2024-12-14 15:05:41 +0300

    Refactor repository

--- src/main/java/com/onboarding/camera/cameraonboarding/repository/LightSensorRepository.java
+++ src/main/java/com/onboarding/camera/cameraonboarding/repository/LightSensorRepository.java
@@ -1,16 +1,13 @@
 package com.onboarding.camera.cameraonboarding.repository;
 
+import com.onboarding.camera.cameraonboarding.entity.Camera;
 import com.onboarding.camera.cameraonboarding.entity.LightSensor;
-import com.onboarding.camera.cameraonboarding.enums.SensorType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface LightSensorRepository extends JpaRepository<LightSensor, UUID> {
 
-    @Query("SELECT s FROM LightSensor s WHERE s.camera.camId = :cameraId AND s.sensorType = :sensorType")
-    List<LightSensor> findByCameraIdAndSensorType(@Param("cameraId") UUID cameraId, @Param("sensorType") SensorType sensorType);
+    List<LightSensor> findLightSensorByCamera(Camera camera);
 }
--- src/main/java/com/onboarding/camera/cameraonboarding/repository/TemperatureSensorRepository.java
+++ src/main/java/com/onboarding/camera/cameraonboarding/repository/TemperatureSensorRepository.java
@@ -1,16 +1,13 @@
 package com.onboarding.camera.cameraonboarding.repository;
 
+import com.onboarding.camera.cameraonboarding.entity.Camera;
 import com.onboarding.camera.cameraonboarding.entity.TemperatureSensor;
-import com.onboarding.camera.cameraonboarding.enums.SensorType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface TemperatureSensorRepository extends JpaRepository<TemperatureSensor, UUID> {
 
-    @Query("SELECT s FROM TemperatureSensor s WHERE s.camera.camId = :cameraId AND s.sensorType = :sensorType")
-    List<TemperatureSensor> findByCameraIdAndSensorType(@Param("cameraId") UUID cameraId, @Param("sensorType") SensorType sensorType);
+    List<TemperatureSensor> findTemperatureSensorByCamera(Camera camera);
 }
