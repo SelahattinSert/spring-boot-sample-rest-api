7643648fba31703c1e3913c5cdd921d391fcd826
Selahattin <98191442+SelahattinSert@users.noreply.github.com>
2025-01-07 02:11:25 +0300

    Add swagger documentation URL

--- src/main/java/com/onboarding/camera/cameraonboarding/config/OpenApiConfig.java
+++ src/main/java/com/onboarding/camera/cameraonboarding/config/OpenApiConfig.java
@@ -15,10 +15,16 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 title = "OpenApi specification",
                 version = "1.0"
         ),
-        servers = @Server(
-                description = "Local ENV",
-                url = "http://localhost:8080"
-        )
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "Swagger UI",
+                        url = "http://localhost:8080/swagger-ui.html"
+                )
+        }
 )
 public class OpenApiConfig {
 }
